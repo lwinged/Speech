5e122d7adef1f84cf37f11e58d5f1e332c01aaa7
flav <flavien.dibello@in-tact.fr>
2018-10-27 09:34:35 +0200

    Initial Commit

---
+++ Speech/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speech
+//
+//  Created by flav on 27/10/2018.
+//  Copyright © 2018 flav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
